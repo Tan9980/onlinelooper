23be54e21aa9a44bf7f94840f3e8655d1f397f16
Tanishq Sinha <tanishq.sinha11@gmail.com>
2024-06-23 19:59:32 +0530

    init
    first build of live looper

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('audio', (data) => {
+        io.emit('audio', data); // Broadcast to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
